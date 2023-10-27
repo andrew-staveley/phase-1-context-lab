6ebfcd1ec35051c3399f37d8a02efd58785e529f
Andrew <andrewstaveley14@gmail.com>
2023-10-26 23:13:48 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* Your Code Here */
-const createEmployeeRecord = function (employeeInformation) {
+const createEmployeeRecord = function(employeeInformation) {
     let employee = {
         firstName: employeeInformation[0],
         familyName: employeeInformation[1],
@@ -10,12 +10,54 @@ const createEmployeeRecord = function (employeeInformation) {
     }
     return employee
 }
-const createEmployeeRecords = function (array) {
+const createEmployeeRecords = function(array) {
     let newArray = []
     for (let line of array) {
         newArray.push(createEmployeeRecord(line))
     }
     return newArray
+}
+function createTimeInEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+    const timeInEvent = {
+      type: 'TimeIn',
+      hour: parseInt(hour),
+      date,
+    };
+    this.timeInEvents.push(timeInEvent);
+    return this;
+}
+function createTimeOutEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(' ')
+    const timeOutEvent = {
+        type: 'TimeOut',
+        hour: parseInt(hour),
+        date,
+    }
+    this.timeOutEvents.push(timeOutEvent)
+    return this
+}
+function hoursWorkedOnDate(soughtDate) {
+    const timeInOnDate = this.timeInEvents.find((event) => event.date === soughtDate)
+    const timeOutOnDate = this.timeOutEvents.find((event) => event.date === soughtDate)
+    const hourIn = timeInOnDate.hour
+    const hourOut = timeOutOnDate.hour
+    return (hourOut - hourIn) / 100
+}
+function wagesEarnedOnDate(soughtDate) {
+    const hoursWorked = hoursWorkedOnDate.call(this, soughtDate)
+    const pay = this.payPerHour
+    return hoursWorked*pay
+}
+function findEmployeeByFirstName(array, firstName) {
+    return array.find((element) => element.firstName === firstName)
+}
+function calculatePayroll(array) {
+    let total = 0
+    array.forEach((employee) => {
+        total += allWagesFor.call(employee)
+    })
+    return total
 }
 /*
  We're giving you this function. Take a look at it, you might see some usage
